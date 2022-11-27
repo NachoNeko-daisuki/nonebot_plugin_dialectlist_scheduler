0a45bb7b4d803bb53b88ab1edf32e44f26a07c21
what does Jiaran eat tonight <41470933+NachoNeko-daisuki@users.noreply.github.com>
2022-11-27 10:47:51 +0800

    没改完先存个档，哪天有空想起来再继续

--- nonebot_plugin_dialectlist/__init__.py
+++ nonebot_plugin_dialectlist/__init__.py
@@ -1,7 +1,8 @@
 import re
 import time
 import asyncio
-from typing import List, Tuple, Union
+import random
+from typing import Dict, Optional, List, Tuple, Union
 from datetime import datetime, timedelta
 
 try:
@@ -26,6 +27,42 @@ from nonebot_plugin_guild_patch import GuildMessageEvent
 from .function import get_message_records,msg_counter, msg_list2msg
 from .config import plugin_config
 
+from typing import Dict, List, Optional, Tuple
+from pathlib import Path
+from datetime import date
+import aiofiles
+import httpx
+
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+    
+config_path = Path("config/everyday_en.json")
+config_path.parent.mkdir(parents=True, exist_ok=True)
+if config_path.exists():
+    with open(config_path, "r", encoding="utf8") as f:
+        CONFIG: Dict[str, List] = json.load(f)
+else:
+    CONFIG: Dict[str, List] = {"opened_groups": []}
+    with open(config_path, "w", encoding="utf8") as f:
+        json.dump(CONFIG, f, ensure_ascii=False, indent=4)
+
+try:
+    scheduler = require("nonebot_plugin_apscheduler").scheduler
+except Exception:
+    scheduler = None
+
+logger.opt(colors=True).info(
+    "已检测到软依赖<y>nonebot_plugin_apscheduler</y>, <g>开启定时任务功能</g>"
+    if scheduler
+    else "未检测到软依赖<y>nonebot_plugin_apscheduler</y>，<r>禁用定时任务功能</r>"
+)
+
+    
+    
+    
 def parse_datetime(key: str):
     """解析数字，并将结果存入 state 中"""
 
@@ -64,7 +101,8 @@ def get_datetime_fromisoformat_with_timezone(date_string: str) -> datetime:
         return datetime.fromisoformat(date_string).astimezone()
 
 
-
+turn_matcher = on_regex(r"^(开启|关闭)定时每日一句([0-9]*)$", priority=999, permission=SUPERUSER)
+list_matcher = on_regex(r"^查看定时每日一句列表$", priority=999, permission=SUPERUSER)
 
 rankings = on_command(
     '群话痨排行榜',
@@ -203,4 +241,92 @@ async def handle_message(
     await asyncio.sleep(1) #让图片先发出来
     if plugin_config.dialectlist_string_suffix_format:
         await rankings.finish(plugin_config.dialectlist_string_suffix_format.format(timecost=time.time()-st-1))
+
+
+@turn_matcher.handle()
+async def _(
+    bot: Bot,
+    event: MessageEvent,
+    matcher: Matcher,
+    args: Tuple[Optional[str], ...] = RegexGroup(),
+):
+    if not scheduler:
+        await matcher.finish("未安装软依赖nonebot_plugin_apscheduler，不能使用定时发送功能")
+    mode = args[0]
+    if isinstance(event, GroupMessageEvent):
+        group_id = args[1] if args[1] else str(event.group_id)
+    else:
+        if args[1]:
+            group_id = args[1]
+        else:
+            await matcher.finish("私聊开关需要输入指定群号")
+    if mode == "开启":
+        if group_id in CONFIG["opened_groups"]:
+            await matcher.finish("该群已经开启，无需重复开启")
+        else:
+            CONFIG["opened_groups"].append(group_id)
+    else:
+        if group_id in CONFIG["opened_groups"]:
+            CONFIG["opened_groups"].remove(group_id)
+        else:
+            await matcher.finish("该群尚未开启，无需关闭")
+    async with lock:
+        async with aiofiles.open(config_path, "w", encoding="utf8") as f:
+            await f.write(json.dumps(CONFIG, ensure_ascii=False, indent=4))
+    await matcher.finish(f"已成功{mode}{group_id}的每日一句")
+
+
+@list_matcher.handle()
+async def _(bot: Bot, event: MessageEvent, matcher: Matcher):
+    if not scheduler:
+        await matcher.finish("未安装软依赖nonebot_plugin_apscheduler，不能使用定时发送功能")
+    msg = "当前打开定时话唠排行的群聊有：\n"
+    for group_id in CONFIG["opened_groups"]:
+        msg += f"{group_id}\n"
+    await matcher.finish(msg.strip())
+        
+        
+async def dialectlist_scheduler(
+    bot: Bot,
+    event: Union[GroupMessageEvent],
+    stop: datetime = Arg(),
+    start: datetime = Arg()
+):
     
+    st = time.time()
+    
+    if isinstance(event,GroupMessageEvent):
+        if plugin_config.dialectlist_excluded_self:
+            bot_id = await bot.call_api('get_login_info')
+            plugin_config.dialectlist_excluded_people.append(str(bot_id["user_id"]))
+        gids:List[str] = [str(event.group_id)]
+        msg_list = await get_message_records(
+            group_ids=gids,
+            exclude_user_ids=plugin_config.dialectlist_excluded_people,
+            message_type='group',
+            time_start=dt.replace(hour=0, minute=0, second=0, microsecond=0),
+            time_stop=state["stop"] - timedelta(days=1)
+        )
+        
+
+
+        
+    msg_dict = await msg_counter(msg_list=msg_list)
+    if isinstance(event,GroupMessageEvent):
+        msg = await msg_list2msg(msg_list=msg_dict,gid=event.group_id,platform='qq',bot=bot,got_num=plugin_config.dialectlist_get_num)
+        
+    await rankings.send(msg)
+    await asyncio.sleep(1) #让图片先发出来
+    if plugin_config.dialectlist_string_suffix_format:
+        await rankings.finish(plugin_config.dialectlist_string_suffix_format.format(timecost=time.time()-st-1))
+    
+    
+if scheduler:
+    hour = env_config.dialectlist_scheduler_hour
+    minute = env_config.dialectlist_scheduler_minute
+    logger.opt(colors=True).info(
+        f"已设定于 <y>{str(hour).rjust(2, '0')}:{str(minute).rjust(2, '0')}</y> 定时发送话唠排行"
+    )
+    scheduler.add_job(
+        post_scheduler, "cron", hour=hour, minute=minute, id="everyday_english"
+    )
